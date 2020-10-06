6e0c5ca531cde42638c1bb7291b85ed1ced12fff
xsnatchysquidx <51396413+xsnatchysquidx@users.noreply.github.com>
2020-10-06 10:48:31 +0300

    adde python sorting algorithms
    added radix, bubble and selection sort in python

---
+++ Sorting/Radix Sort/Python/radix_sort.py
@@ -0,0 +1,16 @@
+def radix_sort(lst, radix=10):
+    max_val = max(lst)
+    power = 0
+    while radix ** power < max_val:
+        power += 1
+    for p in range(power):
+        factor = radix ** p
+        buckets = [list() for _ in range(radix)]
+        for val in lst:
+            tmp = val / factor
+            buckets[int(temp % radix)].append(val)
+        lst = []
+        for b in range(radix):
+            for val in buckets[b]:
+                lst.append(val)
+        return lst
