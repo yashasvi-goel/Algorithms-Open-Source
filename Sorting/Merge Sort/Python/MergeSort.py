5ad0c56a77e333def55254e243ed9c3ccbf1818a
ngalvan <ngalvan@eecs.wsu.edu>
2020-10-19 17:27:19 -0700

    Added comments

--- Sorting/Merge Sort/Python/MergeSort.py
+++ Sorting/Merge Sort/Python/MergeSort.py
@@ -1,12 +1,22 @@
+#MergeSort implementation in Python
 def MergeSort(arr):
     if len(arr) > 1:
+        # Finding the middle of the array
         mid = len(arr)//2
+
+        # Dividing the array into 2 halves
         L = arr[:mid]
         R = arr[mid:]
+
+        # Sorting the first half
         MergeSort(L)
+
+        # Sorting the second half
         MergeSort(R)
+
         i = j = k = 0
 
+        # Copying the element to temp arrays L[] and R[]
         while i < len(L) and j < len(R):
             if L[i] < R[j]:
                 arr[k] = L[i]
@@ -16,6 +26,7 @@ def MergeSort(arr):
                 j += 1
             k += 1
 
+        # Checking if any element was left
         while i < len(L):
             arr[k] = L[i]
             i += 1
@@ -25,7 +36,7 @@ def MergeSort(arr):
             j += 1
             k += 1
 
-
+# Print the list
 def PrintArray(arr):
     for i in range(len(arr)):
         print(arr[i], end = " ")
